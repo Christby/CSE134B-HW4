7fc684fe30561fb676596e5f82fb4a28bc34bb5a
Christby <btian@ucsd.edu>
2023-02-28 10:33:53 -0800

    .

--- customdialog.js
+++ customdialog.js
@@ -1,26 +1,3 @@
-import DOMPurify from 'dompurify';
-
-const createDialog = (title, content, buttons) => {
-  const dialog = document.createElement('dialog');
-  dialog.innerHTML = `
-    <h2>${DOMPurify.sanitize(title)}</h2>
-    <p>${DOMPurify.sanitize(content)}</p>
-  `;
-
-  // Create buttons
-  buttons.forEach((button) => {
-    const btn = document.createElement('button');
-    btn.textContent = button.label;
-    btn.addEventListener('click', () => {
-      button.callback();
-      dialog.close();
-    });
-    dialog.appendChild(btn);
-  });
-
-  // Append to body and open
-  document.body.appendChild(dialog);
-  dialog.showModal();
-};
-
-export default createDialog;
+export function sayHi(user) {
+  return `Hello, ${user}!`;
+}
