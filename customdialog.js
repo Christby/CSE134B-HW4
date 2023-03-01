2d2e4a6d5a66701da65f2f3b016ca6dcfea2d757
Christby <btian@ucsd.edu>
2023-02-28 20:03:45 -0800

    。

--- customdialog.js
+++ customdialog.js
@@ -1,3 +1,62 @@
-export function sayHi(user) {
-  return `Hello, ${user}!`;
+function showAlert(message) {
+  // first, clean the message using DOMPurify
+  let msg = DOMPurify.sanitize(message);
+  const dialogTemplate = document.querySelector('#alert-dialog');
+  const dialog = dialogTemplate.content.cloneNode(true).querySelector('dialog');
+  dialog.querySelector('.header').textContent = 'Alert';
+  dialog.querySelector('.body').textContent = msg;
+  dialog.querySelector('button').addEventListener('click', () => dialog.close());
+  document.body.appendChild(dialog);
+  dialog.showModal();
 }
+
+function showConfirm(message, outputTag) {
+  // first, clean the message using DOMPurify
+  let msg = DOMPurify.sanitize(message);
+  const dialogTemplate = document.querySelector('#confirm-dialog');
+  const dialog = dialogTemplate.content.cloneNode(true).querySelector('dialog');
+  dialog.querySelector('.header').textContent = 'Confirm';
+  dialog.querySelector('.body').textContent = msg;
+  // not sure how to handle the output from here
+  dialog.querySelector('.ok').addEventListener('click', () => {
+    outputTag.textContent = `Confirm result: true`;
+    dialog.close();
+  });
+  dialog.querySelector('.cancel').addEventListener('click', () => {
+    outputTag.textContent = `Confirm result: false`;
+    dialog.close();
+  });
+  document.body.appendChild(dialog);
+  dialog.showModal();
+}
+
+function showPrompt(message, outputTag) {
+  // first, clean the message using DOMPurify
+  let msg = DOMPurify.sanitize(message);
+  const dialogTemplate = document.querySelector('#prompt-dialog');
+  const dialog = dialogTemplate.content.cloneNode(true).querySelector('dialog');
+  dialog.querySelector('.header').textContent = 'Prompt';
+  dialog.querySelector('.body').textContent = msg;
+  let input = document.createElement('input');
+  input.type = 'text';
+  dialog.querySelector('.body').appendChild(input);
+  // not sure how to handle the output from here
+  dialog.querySelector('.ok').addEventListener('click', () => {
+    if (input === null) {
+      outputTag.textContent = 'User didn\'t input his/her name';
+      dialog.close();
+    } else {
+      let cleanInput = DOMPurify.sanitize(input);
+      outputTag.textContent = `Your name is: ${cleanInput}`;
+      dialog.close();
+    }
+  });
+  dialog.querySelector('.cancel').addEventListener('click', () => {
+    outputTag.textContent = `User didn\'t input his/her name`;
+    dialog.close();
+  });
+  document.body.appendChild(dialog);
+  dialog.showModal();
+}
+
+export {showAlert, showConfirm, showPrompt}
